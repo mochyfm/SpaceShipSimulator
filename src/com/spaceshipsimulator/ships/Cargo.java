956efae6b87f615cd1d21adf1ef6f184b0c48eda
Moises Muiña <mochyfm@gmail.com>
2022-04-21 13:28:05 +0100

    Feature: Created on Cargo.java file the attributes (extended from parent class SpaceShip)

--- src/com/spaceshipsimulator/ships/Cargo.java
+++ src/com/spaceshipsimulator/ships/Cargo.java
@@ -1,6 +1,29 @@
 package com.spaceshipsimulator.ships;
 
-public class Cargo {
+public class Cargo extends SpaceShip {
+
+    private int cargo;
+    private int loadQuantity;
+    private String CargoShipASCII = "\t             `. ___\n" +
+            "                    __,' __`.                _..----....____\n" +
+            "        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'\n" +
+            "  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'\n" +
+            ",'________________                          \\`-._`-','\n" +
+            " `._              ```````````------...___   '-.._'-:\n" +
+            "    ```--.._      ,.                     ````--...__\\-.\n" +
+            "            `.--. `-`                       ____    |  |`\n" +
+            "              `. `.                       ,'`````.  ;  ;`\n" +
+            "                `._`.        __________   `.      \\'__/`\n" +
+            "                   `-:._____/______/___/____`.     \\  `\n" +
+            "                               |       `._    `.    \\\n" +
+            "                               `._________`-.   `.   `.___\n" +
+            "                                                  `------'`";
+
+    public Cargo(String name, String galacticLicensePlate, int cargo) {
+        super(name, galacticLicensePlate);
+        this.cargo = cargo;
+        this.loadQuantity = 0;
+    }
 
 
 }
